58ffb4f58cb9c8e8430367870b4205abe10efa98
mengxiangqing <1509067555@qq.com>
2021-11-19 16:14:31 +0800

    2021/11/19

---
+++ .history/18.四数之和_20211119152205.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=18 lang=java
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
+        
+    }
+}
+// @lc code=end
+
---
+++ .history/18.四数之和_20211119155035.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=18 lang=java
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        Arrays.sort(nums);// 排序
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (nums[i] > target)
+                break;
+            if (i > 0 && (nums[i] == nums[i - 1]))
+                continue;
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if ((nums[j] == nums[j - 1]) && j > 1) {// 跳过重复的，j>1确保第一次不被跳过
+                    continue;
+                }
+                int l = j + 1;
+                int r = nums.length - 1;
+                while (l < r) {
+                    int num = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (num == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                    } else if (num < target) {
+                        l++;
+                        if ((nums[l] == nums[l - 1]) && l < r) {
+                            l++;
+                        }
+                    } else {
+                        r--;
+                        if (nums[r] == nums[r + 1] && l < r) {
+                            r--;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return res;
+    }
+}
+// @lc code=end
