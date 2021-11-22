521fd104a8cac1173aea5536174ed580e6ec4670
mengxiangqing <1509067555@qq.com>
2021-11-22 11:36:29 +0800

    2021/11/22

---
+++ .history/384.打乱数组_20211122105531.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=384 lang=java
+ *
+ * [384] 打乱数组
+ */
+
+// @lc code=start
+class Solution {
+
+    public Solution(int[] nums) {
+
+    }
+    
+    public int[] reset() {
+
+    }
+    
+    public int[] shuffle() {
+
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
+// @lc code=end
+
---
+++ .history/384.打乱数组_20211122110916.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=384 lang=java
+ *
+ * [384] 打乱数组
+ */
+
+// @lc code=start
+class Solution {
+    int[] nums;
+    int[] origin;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.origin = nums;
+
+    }
+
+    public int[] reset() {
+        nums = Arrays.copyOfRange(origin, 0, origin.length);
+        return nums;
+
+    }
+
+    public int[] shuffle() {
+        nums = Arrays.copyOfRange(origin, 0, origin.length);
+        ArrayList<Integer> aList = new ArrayList<Integer>();
+        ArrayList<String> planetlist = new ArrayList<String>();
+        String[] planets = {"earth", "mars", "venus", "jupiter"};
+        for(int i=0;i<planets.length;i++)
+        {
+              planetlist.add(planets[i]);        
+        }
+        for (String str: planetlist)
+        {
+              System.out.println(str);
+        }
+
+        return nums;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such: Solution obj =
+ * new Solution(nums); int[] param_1 = obj.reset(); int[] param_2 =
+ * obj.shuffle();
+ */
+// @lc code=end
