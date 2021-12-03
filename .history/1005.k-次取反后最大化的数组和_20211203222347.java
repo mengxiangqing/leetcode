3febd1f6aaef1d0231f7fd0146a975ac380c41e6
mengxiangqing <1509067555@qq.com>
2021-12-03 22:48:29 +0800

    2021/12/3

---
+++ .history/1005.k-次取反后最大化的数组和_20211203222347.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1005 lang=java
+ *
+ * [1005] K 次取反后最大化的数组和
+ */
+
+// @lc code=start
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+
+    }
+}
+// @lc code=end
+
---
+++ 1005.k-次取反后最大化的数组和.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=1005 lang=java
+ *
+ * [1005] K 次取反后最大化的数组和
+ */
+
+// @lc code=start
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int sum=0;
+        int numOfNegative=0;
+        for(int i=0;i<nums.length;++i){
+            if(nums[i]<0)numOfNegative++;
+        }
+        //去判断k与负数个数大小
+        if(numOfNegative>=k){
+            for(int i=0;i<nums.length;i++){
+                if(i<k){
+                nums[i]=-nums[i];
+                }
+                sum+=nums[i];
+            }
+                return sum;
+        }
+        else{//k大
+            //先把负数转正
+            for(int i=0;i<numOfNegative;++i){
+                nums[i]=-nums[i];
+
+            }
+            Arrays.sort(nums);
+            int n1=k-numOfNegative;
+            if(n1%2==0){
+                //偶数不变！
+                for(int i=0;i<nums.length;++i){
+                    sum+=nums[i];
+                }
+                return sum;
+            }else{
+                for(int i=0;i<nums.length;i++){
+                    if(i==0)nums[i]=-nums[i];
+                    sum+=nums[i];
+                }
+                return sum;
+            }
+        }
+        
+    }
+}
+// @lc code=end
+
