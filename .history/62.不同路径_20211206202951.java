6ec7a1231ef8885e7a7224670a831fddf429af74
mengxiangqing <1509067555@qq.com>
2021-12-06 21:02:31 +0800

    2021/12/06

---
+++ .history/62.不同路径_20211206202951.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=java
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int sum = 0;
+
+        return sum;
+
+    }
+}
+// @lc code=end
---
+++ 63.不同路径-ii.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=java
+ *
+ * [63] 不同路径 II
+ */
+
+// @lc code=start
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[] f = new int[m];
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+        f[0] =1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] += f[j - 1];
+                }
+            }
+        }
+        
+        return f[m - 1];
+    }
+
+}
+// @lc code=end
