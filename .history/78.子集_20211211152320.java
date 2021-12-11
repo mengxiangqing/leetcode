40281d9801bbe4dfbac360258ef9e7012a6bb8f8
mengxiangqing <1509067555@qq.com>
2021-12-11 19:43:57 +0800

    2021/12/11

---
+++ .history/78.子集_20211211152320.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=78 lang=java
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> ele = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        
+        return res;
+    }
+}
+// @lc code=end
+
---
+++ .history/78.子集_20211211184859.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=78 lang=java
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> ele = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(ele);
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+        }
+        return res;
+    }
+}
+// @lc code=end
+
