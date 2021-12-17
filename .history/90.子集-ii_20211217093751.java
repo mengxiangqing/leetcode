b49a106d25674c63d4afef9ee5dd99ef9c9cc38f
mengxiangqing <1509067555@qq.com>
2021-12-17 10:08:07 +0800

    2021/12/17 好慢

---
+++ .history/90.子集-ii_20211217093751.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=90 lang=java
+ *
+ * [90] 子集 II
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        return res;
+
+    }
+}
+// @lc code=end
+
---
+++ .history/90.子集-ii_20211217094501.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=90 lang=java
+ *
+ * [90] 子集 II
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> empty_List = new ArrayList<>();
+        //先加入空集
+        res.add(empty_List);
+        for (int num : nums) {
+            List<Integer> newSubsets = new ArrayList<>();
+            for (List<Integer> newSubset : res) {
+                
+            }
+        }
+
+        return res;
+
+    }
+}
+// @lc code=end
+
