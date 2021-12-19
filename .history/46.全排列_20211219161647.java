da92dc33a0c72ab5787e0b5f3298bb482bb68a45
mengxiangqing <1509067555@qq.com>
2021-12-19 20:04:30 +0800

    1111

---
+++ .history/46.全排列_20211219161647.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=46 lang=java
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+
+        return res;
+
+    }
+}
+// @lc code=end
+
