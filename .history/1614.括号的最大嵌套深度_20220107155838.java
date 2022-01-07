8e3db9af8db8039c9a0a39ad17b42eb749953d20
mengxiangqing <1509067555@qq.com>
2022-01-07 16:18:53 +0800

    回归现实

---
+++ .history/1614.括号的最大嵌套深度_20220107155838.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1614 lang=java
+ *
+ * [1614] 括号的最大嵌套深度
+ */
+
+// @lc code=start
+class Solution {
+    public int maxDepth(String s) {
+
+    }
+}
+// @lc code=end
+
---
+++ .history/1614.括号的最大嵌套深度_20220107155933.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1614 lang=java
+ *
+ * [1614] 括号的最大嵌套深度
+ */
+
+// @lc code=start
+class Solution {
+    public int maxDepth(String s) {
+        int numOfResult = 0;
+        
+        return numOfResult;
+
+
+    }
+}
+// @lc code=end
+
