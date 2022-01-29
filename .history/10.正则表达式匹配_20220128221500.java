876467d56bcb2bacce93a8df2798595a1388e084
mengxiangqing <1509067555@qq.com>
2022-01-29 13:02:49 +0800

    test

---
+++ .history/10.正则表达式匹配_20220128221500.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=10 lang=java
+ *
+ * [10] 正则表达式匹配
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int p_len = p.length();
+        int s_len = s.length();
+        if (p_len < s_len && !p.contains("*"))
+            return false;
+        int flag_s = 0;
+        boolean[][] dp = new boolean[s_len][p_len];
+        int flag_p = 0;
+        while (flag_p < p_len && flag_s < s_len) {
+            if (s.charAt(flag_s) == p.charAt(flag_p)) {
+                flag_p++;
+                flag_s++;
+            } else if (s.charAt(flag_s) != p.charAt(flag_p)){
+                if(p.charAt(flag_p) == '*') {
+                    flag_s++;
+                } else if (p.charAt(flag_p) == '.') {
+                    flag_p++;
+                    flag_s++;
+                }
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
