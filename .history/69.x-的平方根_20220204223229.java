4b1692a0b27bef3a655b32a0092df5b82bb2acae
mengxiangqing <1509067555@qq.com>
2022-02-04 22:36:11 +0800

    简单题不简单

---
+++ .history/69.x-的平方根_20220204223229.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=java
+ *
+ * [69] x 的平方根 
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt(int x) {
+        int res = 0;
+        int left = 0;
+        int right = x;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid * mid <= x) {
+                res = left;
+                
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
