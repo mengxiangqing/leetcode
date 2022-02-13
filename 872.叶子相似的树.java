006c1a23ee6eb93da2e7409aa8b3f6cea4649aad
mengxiangqing <1509067555@qq.com>
2022-02-13 17:54:30 +0800

    偷懒偷懒

---
+++ 872.叶子相似的树.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=872 lang=java
+ *
+ * [872] 叶子相似的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+
+    }
+}
+// @lc code=end
+
