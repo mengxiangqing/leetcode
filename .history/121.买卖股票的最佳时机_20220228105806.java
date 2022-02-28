82d449b2f207b7b56c841f10a45d6af283abee9d
mengxiangqing <1509067555@qq.com>
2022-02-28 11:07:35 +0800

    暴力超时，题解很妙

---
+++ .history/121.买卖股票的最佳时机_20220228105806.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=java
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxP = 0;
+        for (int i = 0; i < prices.length-1; i++) {
+            for (int j = i+1; j < prices.length; j++) {
+                if (prices[j] > prices[i])
+                    maxP = Math.max(maxP, prices[j] - prices[i]);
+            }
+        }
+        return maxP;
+    }
+}
+// @lc code=end
+
