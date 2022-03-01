17e94e1e986e632218e4349d70b4650706c49ea3
mengxiangqing <1509067555@qq.com>
2022-03-01 08:42:38 +0800

    自己做的差点道行

---
+++ .history/198.打家劫舍_20220301082005.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=java
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        int count = 0;
+
+        return count;
+    }
+}
+// @lc code=end
+
---
+++ .history/198.打家劫舍_20220301082141.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=java
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        //f[i]代表到i的最大值
+        int[] f = new int[n];
+
+        return count;
+    }
+}
+// @lc code=end
+
