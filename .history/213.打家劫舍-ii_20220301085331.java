dd63b6a914ead101f1561c42533c034c2efe62cc
mengxiangqing <1509067555@qq.com>
2022-03-01 09:12:51 +0800

    okk

---
+++ .history/213.打家劫舍-ii_20220301085331.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=java
+ *
+ * [213] 打家劫舍 II
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length <= 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        if (nums.length == 3) {
+            int max = 0;
+            for (int i : nums) {
+                max = Math.max(max, i);
+
+            }
+        }
+        int n = nums.length;
+        int[] f = new int[n];
+        f[0] = nums[0];
+        f[1] = Math.max(f[0], nums[1]);
+        for (int i = 2; i < f.length - 1; i++) {
+            f[i] = Math.max(f[i - 2] + nums[i], f[i - 1]);
+
+        }
+        return f[n - 1];
+
+    }
+}
+// @lc code=end
+
