ef0e9fbaf6bbbdf7162fd966a43d4ce547b315a3
mengxiangqing <1509067555@qq.com>
2022-03-24 21:13:54 +0800

    bfs

---
+++ .history/102.二叉树的层序遍历_20220324201901.java
@@ -0,0 +1,57 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=102 lang=java
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addFirst(root);
+        while (!queue.isEmpty()) {
+            TreeNode p=queue.removeLast();
+            List<Integer> path = new ArrayList<>();
+            if (p.left != null) {
+                queue.addFirst(p.left);
+            }
+            if (p.right != null) {
+                queue.addFirst(p.right);
+            }
+
+        }
+        return res;
+
+    }
+}
+// @lc code=end
--- .history/剑指offer_20220324113559.java
+++ .history/剑指offer_20220324113559.java
@@ -98,12 +98,3 @@ class CopyComplixList {
         return newDummy.next;
     }
 }
-
-class StringReverse {
-    public String reverseLeftWords(String s, int n) {
-        StringBuilder sb = new StringBuilder(s.substring(n));
-        sb.append(s.substring(0, n));
-        return sb.toString();
-    }
-
-}
