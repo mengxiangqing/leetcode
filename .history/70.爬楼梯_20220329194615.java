3508727b791d56194789b279c8e91a2f13493e87
mengxiangqing <1509067555@qq.com>
2022-03-29 20:40:33 +0800

    nice

---
+++ .history/70.爬楼梯_20220329194615.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=java
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[Math.max(n + 1, 3)];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
+    }
+}
+// @lc code=end
