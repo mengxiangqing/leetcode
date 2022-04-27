17a7feb2f1774692530b092fc22b6587089bc9b7
mengxiangqing <1509067555@qq.com>
2022-04-27 10:42:19 +0800

    dfs

---
+++ .history/417.太平洋大西洋水流问题_20220427083956.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=417 lang=java
+ *
+ * [417] 太平洋大西洋水流问题
+ */
+
+// @lc code=start
+class Solution {
+    boolean[][] isLeftTop;
+    boolean[][] isTightDown;
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        List<List<Integer>> res = new ArrayList<>();
+
+        return res;
+    }
+}
+// @lc code=end
+
---
+++ .history/417.太平洋大西洋水流问题_20220427102156.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=417 lang=java
+ *
+ * [417] 太平洋大西洋水流问题
+ */
+
+// @lc code=start
+class Solution {
+    // leftTop[i][j]表示能否流向左上边,0 未赋值，1 可以，2 不可以
+    private short[][] isLeftTop;
+    // 表示能否流向右下边
+    private short[][] isTightDown;
+
+    private int[][] direction = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        List<List<Integer>> res = new ArrayList<>();
+        isLeftTop = new short[m][n];
+        isTightDown = new short[m][n];
+        return res;
+    }
+
+    public static void exploer(int i, int j, int[][] heights) {
+
+    }
+}
+// @lc code=end
