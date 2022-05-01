dd2947692d6b32b762de31ef2c35ec23ac21e332
mengxiangqing <1509067555@qq.com>
2022-05-01 23:20:57 +0800

    这都能超过百分之九十多？？

---
+++ .history/1305.两棵二叉搜索树中的所有元素_20220501231439.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=1305 lang=java
+ *
+ * [1305] 两棵二叉搜索树中的所有元素
+ */
+
+/**
+ * Definition for a binary tree node.
+ */
+ private class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+// @lc code=start
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+
+    }
+}
+// @lc code=end
+
