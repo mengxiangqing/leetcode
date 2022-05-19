a811ca85bbc77cc0283040d9440af6ecad165a5a
mengxiangqing <1509067555@qq.com>
2022-05-19 21:10:59 +0800

    开始尝试c++

---
+++ leetcode/168.excel表列名称.cpp
@@ -0,0 +1,27 @@
+#include<string>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=168 lang=cpp
+ *
+ * [168] Excel表列名称
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+       string ans;
+        while (columnNumber>0)
+        {
+            columnNumber--;
+            ans+=columnNumber%26+'A';
+            columnNumber/=26;
+
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
+// @lc code=end
+
