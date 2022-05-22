4c9edff05e8f77dcdb69dd379cf4f76c46d105c7
mengxiangqing <1509067555@qq.com>
2022-05-22 12:53:49 +0800

    啥也不是

--- test.java
+++ test.java
@@ -9,40 +9,20 @@ import java.util.List;
 
 public class test {
 
-    public static void main(String[] args) {
-
-        // int a[][] = {{1,1,0,0},{0,0,1,1},{1,1,0,0},{0,0,1,1}};
-        // foooo f = new foooo();
-        // f.construct(a);
-        Solution942 s = new Solution942();
-        int nums[] = { 10, 5, 2, 6 };
-        String start = "AACCGGTT", end = "AAACGGTA";
-        String[] bank = { "AACCGGTA", "AACCGCTA", "AAACGGTA" };
-        s.diStringMatch("IDID");
-
-    }
-
-    public static int[] nextLargerNodes() {
-        List<Integer> list = new ArrayList<>(List.of(2, 1, 5));
-        // 链表转数组
-        // System.out.println(Arrays.toString(list.toArray()));
-        // 存放数值和下标的栈 【val,index】
-        Deque<int[]> que = new ArrayDeque<>();
-        que.addFirst(new int[] { list.get(0), 0 });
-        int[] res = new int[list.size()];
-        for (int i = 1; i < res.length; i++) {
-            if (list.get(i) <= que.getFirst()[0])
-                que.addFirst(new int[] { list.get(i), i });
-            else {
-                while (!que.isEmpty() && list.get(i) > que.getFirst()[0]) {
-                    res[que.getFirst()[1]] = list.get(i);
-                    que.removeFirst();
-                }
-                que.addFirst(new int[] { list.get(i), i });
-            }
+        public static void main(String[] args)
+        {
+            int x, y;
+            x = 5 >> 2;
+            y = x >>> 2;
+            System.out.println(y);
+            String str="hello";
+            str+='a';
+            str+=100;
+            System.out.println(str);
+         
         }
-        return res;
-    }
+    
+    
 
 }
 
