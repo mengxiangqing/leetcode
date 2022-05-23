d9b8def2f2c1565a60e4bfac1ab6c99a30917ccd
mengxiangqing <1509067555@qq.com>
2022-05-23 09:18:36 +0800

    哦豁

---
+++ leetcode/108.将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+/**
+ * Definition for a binary tree node.
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lc code=start
+class Solution
+{
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        return sortedArray(nums, 0, nums.size() - 1);
+    }
+    TreeNode *dfs(vector<int> &nums, int left, int right)
+    {
+        if (left > right)
+        {
+            return nullptr;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = dfs(nums, left, mid - 1);
+        root->right = dfs(nums, mid + 1, right);
+        return root;
+    }
+  
+};
+// @lc code=end
