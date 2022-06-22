f452b0001d445953ad4789a6cbd77af915eed50f
mengxiangqing <1509067555@qq.com>
2022-06-22 20:52:40 +0800

    还是用java做题

---
+++ 95.不同的二叉搜索树-ii.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=95 lang=java
+ *
+ * [95] 不同的二叉搜索树 II
+ */
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+
+    }
+}
+// @lc code=end
