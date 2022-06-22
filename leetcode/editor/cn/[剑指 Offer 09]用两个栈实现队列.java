8d580482c79ce0811e2649b6ec31a90d367286e3
mengxiangqing <1509067555@qq.com>
2022-06-22 22:04:17 +0800

    ？？？

---
+++ leetcode/editor/cn/[剑指 Offer 09]用两个栈实现队列.java
@@ -0,0 +1,65 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+//用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的
+//功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+//
+//
+//
+// 示例 1：
+//
+// 输入：
+//["CQueue","appendTail","deleteHead","deleteHead"]
+//[[],[3],[],[]]
+//输出：[null,null,3,-1]
+//
+//
+// 示例 2：
+//
+// 输入：
+//["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+//[[],[],[5],[2],[],[]]
+//输出：[null,-1,null,null,5,2]
+//
+//
+// 提示：
+//
+//
+// 1 <= values <= 10000
+// 最多会对 appendTail、deleteHead 进行 10000 次调用
+//
+// Related Topics 栈 设计 队列 👍 554 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class CQueue {
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new ArrayDeque<>();
+        stack2 = new ArrayDeque<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.addFirst(value);
+    }
+
+    public int deleteHead() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.addFirst(stack1.removeLast());
+            }
+        }
+        return stack2.isEmpty() ? -1 : stack2.removeLast();
+
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
