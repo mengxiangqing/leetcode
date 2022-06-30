8f7f1d8b5f3b6d443a9438d70e5388c9214acaaa
mengxiangqing <1509067555@qq.com>
2022-06-30 22:24:45 +0800

    就这吧

--- 程序员面试金典.java
+++ 程序员面试金典.java
@@ -2,6 +2,7 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -41,6 +42,37 @@ class ListNode {
 }
 
 class Solutio {
+
+  // 面试题 02.08. 环路检测
+  // 给定一个链表，如果它是有环链表，实现一个算法返回环路的开头节点。若环不存在，请返回 null。
+  // 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+  public ListNode detectCycle(ListNode head) {
+    ListNode p = head;
+    ListNode q = head;
+    while (q.next != null && q.next.next!=null) {
+      q = q.next.next;
+      p = p.next;
+      if (p == q)
+        return p;
+    }
+    return null;
+  }
+
+  // 面试题 02.07. 链表相交
+  // 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+
+  // 涨涨记性吧
+  // 浪漫相遇题
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    if (headA == null || headB == null) return null;
+    ListNode p = headA;
+    ListNode q = headB;
+    while (p != q) {
+      p = p != null ? p.next : headB;
+      q = q != null ? q.next : headA;
+    }
+    return p;
+  }
 
   //翻转链表
   public ListNode reverseList(ListNode head) {
