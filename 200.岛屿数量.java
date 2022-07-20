f046daabad8af7d299db47dc99a7f733b64b625a
mengxiangqing <1509067555@qq.com>
2022-07-20 21:53:48 +0800

    并查集

--- 200.岛屿数量.java
+++ 200.岛屿数量.java
@@ -1,6 +1,3 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 /*
  * @lc app=leetcode.cn id=200 lang=java
  *
@@ -9,63 +6,59 @@ import java.util.Queue;
 
 // @lc code=start
 class Solution {
+    int[] p;
+    int res;
+
+    int find(int i) {
+        if (p[i] == i)
+            return p[i];
+        return p[i] = find(p[i]);
+    }
+
+    void union(int i, int j) {
+        if (find(i) == find(j)) {
+            return; // 避免重复合并操作
+        }
+        p[find(i)] = p[find(j)];
+        res--;
+    }
+
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int numOfLands = 0;
+        res = 0;
+        p = new int[m * n];
+
+        // 初始化parent数组
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                int idx = i * n + j;
+                p[idx] = idx;
                 if (grid[i][j] == '1') {
-                    numOfLands++;
-                    // dfs(grid, i, j);
-                    grid[i][j] = '0';
-                    Queue<Integer> neighbors = new LinkedList<>();
-                    neighbors.add(i * n + j);
-                    while (!neighbors.isEmpty()) {
-                        int id = neighbors.remove();
-                        int row = id / n;
-                        int col = id % n;
-                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
-                            neighbors.add((row - 1) * n + col);
-                            grid[row - 1][col] = '0';
-                        }
-                        if (row + 1 < m && grid[row + 1][col] == '1') {
-                            neighbors.add((row + 1) * n + col);
-                            grid[row + 1][col] = '0';
-                        }
-                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
-                            neighbors.add(row * n + col - 1);
-                            grid[row][col - 1] = '0';
-
-                        }
-                        if (col + 1 < n && grid[row][col + 1] == '1') {
-                            neighbors.add(row * n + col + 1);
-                            grid[row][col + 1] = '0';
-
-                        }
-
-                    }
+                    res++;
                 }
-
             }
         }
 
-        return numOfLands;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int idx = i * n + j;
+                if (grid[i][j] == '1') {
+                    // 合并岛屿，从左上角开始检查右 和 下。
+                    if (i + 1 < m && grid[i + 1][j] == '1') {
+                        union(idx, (i + 1) * n + j);
+                    }
+                    if (j + 1 < n && grid[i][j + 1] == '1') {
+                        union(idx, i * n + j + 1);
+                    }
 
-    }
-
-    // dfs，深度优先遍历，将遇到的岛屿都变成海
-    public void dfs(char[][] grid, int ix, int jx) {
-        int m = grid.length;
-        int n = grid[0].length;
-        if (ix < 0 || jx < 0 || ix >= m || jx >= n || grid[ix][jx] == '0') {
-            return;
+                }
+            }
         }
-        grid[ix][jx] = '0';
-        dfs(grid, ix - 1, jx);
-        dfs(grid, ix + 1, jx);
-        dfs(grid, ix, jx - 1);
-        dfs(grid, ix, jx + 1);
+
+        return res;
+
     }
+
 }
 // @lc code=end
