5f6b36f02ed185940530be4102ce66f3fb546290
mengxiangqing <1509067555@qq.com>
2022-07-30 23:36:16 +0800

    不难

--- 236.二叉树的最近公共祖先.java
+++ 236.二叉树的最近公共祖先.java
@@ -1,5 +1,11 @@
-import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /*
  * @lc app=leetcode.cn id=236 lang=java
@@ -41,6 +47,44 @@ class Solution {
 
     }
 
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        // key 是节点，value是父亲
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        // 层序遍历，建立父子关系
+        Deque<TreeNode> que = new ArrayDeque<>();
+        parentMap.put(root, null);
+        que.addLast(root);
+        while (!que.isEmpty()) {
+            int curSize = que.size();
+            for (int i = 0; i < curSize; i++) {
+                TreeNode node = que.removeLast();
+                if (node.left != null) {
+                    parentMap.put(node.left, node);
+                    que.addLast(node.left);
+                }
+                if (node.right != null) {
+                    parentMap.put(node.right, node);
+                    que.addLast(node.right);
+                }
+            }
+        }
+
+        Set<TreeNode> temp = new HashSet<>();
+        // 将p的族谱收集下来
+        while (p != null) {
+            temp.add(p);
+            p = parentMap.get(p);
+        }
+        while (q != null) {
+
+            if (temp.add(q) == false) {
+                return q;
+            }
+            q = parentMap.get(q);
+
+        }
+        return root;
+    }
 
 }
 // @lc code=end
